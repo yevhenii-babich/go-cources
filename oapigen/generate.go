470cfc45799f5fa804278110f4203d564e914229
Yevhenii Babich <yevhenii.babich@globallogic.com>
2021-04-28 18:37:08 +0300

    generate example

--- oapigen/generate.go
+++ oapigen/generate.go
@@ -3,4 +3,4 @@ package main
 //go:generate go get github.com/deepmap/oapi-codegen/cmd/oapi-codegen
 //go:generate oapi-codegen -generate types -o ./service/models/models.gen.go -package models sample.yaml
 //go:generate oapi-codegen -generate server -o server.gen.go -package main -import-mapping=sample.yaml:github.com/yevhenii-babich/go-cources/oapigen/service/models sample.yaml
-//go:generate
+
